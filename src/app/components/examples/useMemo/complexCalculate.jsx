ba5ffd1fa5c709ed477348a43d0066b713431de8
m33rzoev <84869826+m33rzoev@users.noreply.github.com>
2023-02-22 17:31:09 +0300

    add some

--- src/app/components/examples/useMemo/complexCalculate.jsx
+++ src/app/components/examples/useMemo/complexCalculate.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 
+function factorial(n) {
+    return n ? n * factorial(n - 1) : 1;
+}
+
 const ComplexCalculateExample = () => {
+    const fact = factorial(100);
     return (
         <>
             <CardWrapper>
                 <SmallTitle>Кэширование сложных вычислений</SmallTitle>
+                <p>Result: {fact}</p>
             </CardWrapper>
             <CardWrapper>
                 <SmallTitle>Зависимость от сторонних setState</SmallTitle>
--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
 const ProgrammableActionsExample = () => {
+    const inputRef = useRef();
+    const handleClick = () => {
+        console.log(inputRef.current.clientWidth);
+        inputRef.current.focus();
+    };
+    const handleClickWidth = () => {
+      inputRef.current.style.width = "200px";
+    };
     return (
         <CardWrapper>
             <SmallTitle className="card-title">
                 Программируемые действия и свойства
             </SmallTitle>
+            <Divider />
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input ref={inputRef} type="email" className="form-control" id="email" />
+            <button className="btn btn-primary" onClick={handleClick}>Фокус</button>
+            <button className="btn btn-secondary m-lg-1" onClick={handleClickWidth}>Изменить ширину</button>
         </CardWrapper>
     );
 };
