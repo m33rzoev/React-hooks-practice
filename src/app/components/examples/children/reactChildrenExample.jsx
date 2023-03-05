e8789ff4cbe44cd2d32925bfc427722440de62db
m33rzoev <84869826+m33rzoev@users.noreply.github.com>
2023-03-05 21:28:32 +0300

    add new files

--- src/app/components/examples/children/reactChildrenExample.jsx
+++ src/app/components/examples/children/reactChildrenExample.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
+import TextField from "../../common/form/textField";
+
+const FormComponent = ({ children }) => {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+    const handleChange = (target) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: [target.value]
+        }));
+    };
+    return React.Children.map(children, (child) => {
+        const config = {
+            ...child.props,
+            onChange: handleChange,
+            value: data[child.props.name] || ""
+        };
+        return React.cloneElement(child, config);
+    });
+};
 const ReactChildrenExample = () => {
     return (
         <CardWrapper>
             <SmallTitle>Clone form and add props</SmallTitle>
+            <Divider />
+            <FormComponent>
+                <TextField name="email" label="email" />
+                <TextField name="password" label="Пароль" type="password" />
+            </FormComponent>
         </CardWrapper>
     );
 };
--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    const inputRef = useRef();
+    const handleChange = () => {
+        inputRef.current.innerText = "text";
+        inputRef.current.style.width = "150px";
+        inputRef.current.style.height = "80px";
+    };
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -12,6 +18,7 @@ const UseRefExercise = () => {
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
             <div
+                ref={inputRef}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
                     height: 40,
@@ -20,6 +27,14 @@ const UseRefExercise = () => {
                 }}
             >
                 <small>Блок</small>
+            </div>
+            <div>
+                <button
+                    className="btn btn-secondary mt-2"
+                    onClick={handleChange}
+                >
+                    Trigger Action
+                </button>
             </div>
         </CollapseWrapper>
     );
